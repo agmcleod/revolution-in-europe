52a779d31af5859f58bd839e0f0db80256829a69
Aaron McLeod <sircoolguy99@gmail.com>
2013-06-22 14:40:54 -0400

    added in mobile defaults.

--- scripts/app.js
+++ scripts/app.js
@@ -39,4 +39,29 @@ Game.Loader = (function() {
 window.onReady(function() {
   var loader = new Game.Loader();
   loader.init();
+  if (me.sys.isMobile && !navigator.isCocoonJS) {
+    // Prevent the webview from moving on a swipe
+    window.document.addEventListener("touchmove", function (e) {
+      e.preventDefault();
+      window.scroll(0, 0);
+      return false;
+    }, false);
+
+    // Scroll away mobile GUI
+    (function () {
+      window.scrollTo(0, 1);
+    }).defer();
+
+    me.event.subscribe(me.event.WINDOW_ONRESIZE, function (e) {
+      window.scrollTo(0, 1);
+    })
+
+    // Retina viewport settings
+    if (window.devicePixelRatio > 1) {
+      document.getElementById("viewport").setAttribute(
+        "content",
+        "width=device-width, initial-scale=0.5, maximum-scale=0.5, user-scalable=0"
+      );
+    }
+  }
 });
